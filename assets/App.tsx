a15771c3922900e2172ab9c5ee6c1637d55a547f
VsNik <VsNik.10@gmail.com>
2020-10-02 19:33:02 +0300

    add react and typescript

---
+++ assets/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const App: React.FC = (): React.ReactElement => {
+
+    return (
+        <div>
+            <h1>Hello symfony react typescript</h1>
+        </div>
+    );
+};
